788d9f5ba89ee9063b9be036a46d73654f69fce0
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2014-05-21 21:16:09 +0300

    upgrade elasticsearch to 1.1.1

--- esearch/build.sh
+++ esearch/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.6.deb
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.6.noarch.rpm
+#wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.6.noarch.rpm
 docker build -t e_search_centos .
