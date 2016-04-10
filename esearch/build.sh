1242fc803a118bf002574efb7c699cebc6644e5b
Radu Ciorba <radu@presslabs.com>
2016-04-10 19:27:29 +0300

    upgrade es image

--- esearch/build.sh
+++ esearch/build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-if [ ! -f elasticsearch-1.4.4.deb ]; then
-    wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.deb
+if [ ! -f elasticsearch-2.1.1.deb ]; then
+    wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.1.1/elasticsearch-2.1.1.deb
+    # wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.deb
 fi
-sum="`sha1sum elasticsearch-1.4.4.deb`"
-expected_sum="63471c1e9d5f6c5cf2c4d5fe20a9c27c86720810  elasticsearch-1.4.4.deb"
+sum="`sha1sum elasticsearch-2.1.1.deb`"
+expected_sum="6d0d6c61a10e380f1c5f2f4a9d15f8c2d52c48e0  elasticsearch-2.1.1.deb"
 
 if [ "$sum" != "$expected_sum" ]; then
     echo "Checksum validation error. Got:"
     echo $sum
     echo "Expected:"
-    echo $expected_sum
+    echo "'${expected_sum}'"
     exit 1
 fi
 docker build -t rciorba/esearch .
